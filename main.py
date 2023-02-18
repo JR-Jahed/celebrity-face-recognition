984fa6144954679112215d0df1bcd9e81043efe6
JR-Jahed <jrjahed100@gmail.com>
2023-02-18 20:49:36 +0600

    vgg16 acc = 96.71 val_acc = 87.05

--- main.py
+++ main.py
@@ -1,103 +1,9 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
 from tensorflow import keras
-from keras.layers import Conv2D, Dense, MaxPooling2D, Flatten
+from model_vgg16 import ModelVGG16
 
-model_path = "./Model/my_model.h5"
 dataset_path = "./Dataset"
-image_width = 175
-image_height = 175
-batch_size = 32
-
-
-def train(model, training_data, validation_data):
-
-    early_stopping = keras.callbacks.EarlyStopping(
-        patience=5,
-        monitor="val_accuracy",
-        mode="max"
-    )
-    model_ckpt = keras.callbacks.ModelCheckpoint(
-        model_path,
-        monitor="val_accuracy",
-        save_best_only=True,
-        mode="max"
-    )
-
-    model.compile(
-        optimizer='adam',
-        loss=keras.losses.SparseCategoricalCrossentropy(),
-        metrics=['accuracy']
-    )
-
-    history = model.fit(
-        training_data,
-        validation_data=validation_data,
-        epochs=10,
-        callbacks=[early_stopping, model_ckpt]
-    )
-
-    plt.plot(history.history["accuracy"])
-    plt.plot(history.history["val_accuracy"])
-    plt.show()
-
-
-def test(testing_data):
-    model = keras.models.load_model(model_path)
-
-    predictions = model.predict(testing_data)
-
-    correct_guess(test_images, predictions)
-
-    while True:
-        num = input("Enter a number between 0 and {}: ".format(test_images.samples - 1))
-        num = int(num)
-
-        if num == -1:
-            break
-
-        elif 0 <= num < test_images.samples:
-
-            x = int(num / batch_size)
-            y = num % batch_size
-
-            plt.figure()
-            plt.title(classes[np.argmax(predictions[num])])
-            plt.imshow(test_images[x][0][y])
-            plt.axis("off")
-            plt.show()
-
-        else:
-            print("Please enter a correct number")
-
-
-def show_image(image):
-    plt.figure()
-    plt.imshow(image)
-    plt.axis("off")
-
-
-def correct_guess(test_images, predictions):
-
-    correct = 0
-    incorrect = 0
-
-    for i in range(len(test_images)):
-        for j in range(len(test_images[i][1])):
-
-            idx = i * 32 + j
-
-            label = test_images[i][1][j]
-            pred_label = np.argmax(predictions[idx])
-
-            if label == pred_label:
-                correct += 1
-            else:
-                incorrect += 1
-
-    print('correct: ', correct, 'incorrect: ', incorrect)
-
+image_width = 150
+image_height = 150
 
 training = True
 
@@ -106,57 +12,47 @@ if __name__ == "__main__":
 
     image_size = (image_width, image_height)
     image_gen = keras.preprocessing.image.ImageDataGenerator(
-        rescale = 1 / 255.,
-        validation_split = .2
+        rescale=1 / 255.,
+        validation_split=.2
     )
 
     train_images = image_gen.flow_from_directory(
         dataset_path,
-        target_size = image_size,
-        class_mode = 'sparse',
-        subset = 'training'
+        target_size=image_size,
+        class_mode='sparse',
+        subset='training'
     )
 
     test_images = image_gen.flow_from_directory(
         dataset_path,
-        target_size = image_size,
-        class_mode = 'sparse',
-        subset = 'validation',
-        shuffle = False
+        target_size=image_size,
+        class_mode='sparse',
+        subset='validation',
+        shuffle=False
     )
 
     classes = {v: k for k, v in train_images.class_indices.items()}
 
-    model = keras.models.Sequential([
-        Conv2D(32, (3, 3), activation='elu', input_shape=(image_width, image_height, 3), padding='same'),
-        MaxPooling2D(),
+    model = ModelVGG16(
+        train_images=train_images,
+        test_images=test_images,
+        classes=classes,
+        input_shape=(image_width, image_height, 3)
+    )
 
-        Conv2D(32, (3, 3), activation='elu', padding='same'),
-        MaxPooling2D(),
+    model.add_layer(keras.layers.Dense(256, activation='relu'))
+    model.add_layer(keras.layers.Dropout(.2))
+    model.add_layer(keras.layers.Dense(len(classes), activation='softmax'))
 
-        Conv2D(64, (3, 3), activation='elu', padding='same'),
-        MaxPooling2D(),
+    model.conv_base.trainable = False
 
-        Conv2D(64, (3, 3), activation='elu', padding='same'),
-        MaxPooling2D(),
-
-        Conv2D(128, (3, 3), activation='elu', padding='same'),
-        MaxPooling2D(),
-
-        Flatten(),
-
-        Dense(128, activation='elu'),
-
-        Dense(len(classes), activation='softmax')
-    ])
-
-    model.summary()
+    model.model.summary()
 
     if training:
-        train(model, train_images, test_images)
-        test(test_images)
+        model.train(10)
+        model.test()
     else:
-        test(test_images)
+        model.test()
 
 
 
---
+++ model_vgg16.py
@@ -0,0 +1,118 @@
+from tensorflow import keras
+from keras.applications import VGG16
+from keras.layers import Flatten
+import matplotlib.pyplot as plt
+import numpy as np
+
+model_path = "./Models/my_model_vgg16.h5"
+batch_size = 32
+
+
+def show_image(image, title):
+    plt.figure()
+    plt.title(title)
+    plt.imshow(image)
+    plt.axis("off")
+    plt.show()
+
+
+class ModelVGG16:
+    def __init__(self, train_images, test_images, classes, input_shape):
+
+        self.conv_base = VGG16(
+            weights="imagenet",
+            include_top=False,
+            input_shape=input_shape
+        )
+        self.model = keras.models.Sequential([
+            self.conv_base,
+            Flatten()
+        ])
+
+        self.train_images = train_images
+        self.test_images = test_images
+
+        self.classes = classes
+
+    def add_layer(self, layer):
+        self.model.add(layer)
+
+    def train(self, epochs):
+
+        early_stopping = keras.callbacks.EarlyStopping(
+            patience=5,
+            monitor="val_accuracy",
+            mode="max"
+        )
+        model_ckpt = keras.callbacks.ModelCheckpoint(
+            model_path,
+            monitor="val_accuracy",
+            save_best_only=True,
+            mode="max"
+        )
+
+        self.model.compile(
+            optimizer='adam',
+            loss=keras.losses.SparseCategoricalCrossentropy(),
+            metrics=['accuracy']
+        )
+
+        history = self.model.fit(
+            self.train_images,
+            validation_data=self.test_images,
+            epochs=epochs,
+            callbacks=[early_stopping, model_ckpt]
+        )
+
+        epochs_range = range(1, len(history.history['accuracy']) + 1)
+
+        plt.title('Training and Validation accuracy')
+        plt.xticks(epochs_range)
+        plt.plot(epochs_range, history.history["accuracy"], 'bo', label='Training acc')
+        plt.plot(epochs_range, history.history["val_accuracy"], 'b', label='Validation acc')
+        plt.legend()
+        plt.show()
+
+    def test(self):
+        model = keras.models.load_model(model_path)
+
+        predictions = model.predict(self.test_images)
+
+        self.correct_guess(predictions)
+
+        while True:
+            num = input("Enter a number between 0 and {}: ".format(self.test_images.samples - 1))
+            num = int(num)
+
+            if num == -1:
+                break
+
+            elif 0 <= num < self.test_images.samples:
+
+                x = int(num / batch_size)
+                y = num % batch_size
+
+                show_image(self.test_images[x][0][y], self.classes[np.argmax(predictions[num])])
+
+            else:
+                print("Please enter a correct number")
+
+    def correct_guess(self, predictions):
+
+        correct = 0
+        incorrect = 0
+
+        for i in range(len(self.test_images)):
+            for j in range(len(self.test_images[i][1])):
+
+                idx = i * 32 + j
+
+                label = self.test_images[i][1][j]
+                pred_label = np.argmax(predictions[idx])
+
+                if label == pred_label:
+                    correct += 1
+                else:
+                    incorrect += 1
+
+        print('correct: ', correct, 'incorrect: ', incorrect)
